e93075d10accd7a6695bee507d4ff928202cac90
Wilson Lucena <wilsonllucena@gmail.com>
2021-12-18 17:44:59 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adpater.spec.ts
+++ src/utils/email-validator-adpater.spec.ts
@@ -1,9 +1,18 @@
 import { EmailValidatorAdapater } from './email-validator-adpater'
+import validator from 'validator'
 
 describe('EmailValidator Adapater', () => {
   test('should return false if validator return false', () => {
     const sut = new EmailValidatorAdapater()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
+  })
+
+  test('should return true if validator return true', () => {
+    const sut = new EmailValidatorAdapater()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(true)
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
   })
 })
