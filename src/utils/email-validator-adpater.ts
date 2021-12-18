344301063cdc1debde69ac921774a5d3a4d7bd5f
Wilson Lucena <wilsonllucena@gmail.com>
2021-12-18 17:45:08 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adpater.ts
+++ src/utils/email-validator-adpater.ts
@@ -1,7 +1,8 @@
 import { EmailValidator } from '../presentation/protocols/email-validator'
-
+import validator from 'validator'
 export class EmailValidatorAdapater implements EmailValidator {
   isValid (email: string): boolean {
-    return false
+    console.log(email)
+    return validator.isEmail(email)
   };
 }
