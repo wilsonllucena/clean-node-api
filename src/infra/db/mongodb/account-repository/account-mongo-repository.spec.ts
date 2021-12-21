1f811f48c2adb666daa64a3f94cdb35bbb81f2c7
Wilson Lucena <wilsonllucena@gmail.com>
2021-12-21 18:56:39 -0300

    test:  ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from "../helpers/mongo-helper";
+import { AccountMongoRepository } from "./account-mongo-repository";
+
+describe('Account Mongo Repositoy', () => {
+    
+    beforeAll(async () => {
+       await MongoHelper.connect(process.env.MONGO_URL)
+    })
+    afterAll(async () => {
+        await MongoHelper.disconnect();
+    })
+  test('Should return an account on sucess', async () => {
+    const sut = new AccountMongoRepository();
+    const account = await sut.add({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+    })
+
+    // Como não sabe onumero do id que será gerado pelo mongodb, separar validações
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
