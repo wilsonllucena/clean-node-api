557420fec6c9218b7e57a189af01b3492e557516
Wilson Lucena <wilsonllucena@gmail.com>
2021-12-27 21:10:45 -0300

    refactor: coonec to mongodb before to access

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,15 +1,31 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('SignUP Routes', () => {
-  test('Should return an account on success', async () => {
-    await request(app).post('/api/signup')
-    .send({
-        name: 'Wilson',
-        email: 'wilson@email.com',
-        password: '123',
-        passwordConfirmation: '123'
+    //Connecta no banco quando iniciar os testes
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL);
+    });
+
+    //Desconnecta do banco no final dos testes
+    afterAll(async () => {
+        await MongoHelper.disconnect();
+    });
+
+    //Limpar tabelas no teste
+    beforeEach(async () => {
+        const accountCollection = MongoHelper.getCollection("accounts");
+        await accountCollection.deleteMany({});
+    });
+    test('Should return an account on success', async () => {
+        await request(app).post('/api/signup')
+            .send({
+                name: 'Wilson',
+                email: 'wilson@email.com',
+                password: '123',
+                passwordConfirmation: '123'
+            })
+            .expect(200)
     })
-    .expect(200)
-  })
 })
