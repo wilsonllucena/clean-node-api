55cabaa1a6046096109849cb6d048c955402ac5c
Wilson Lucena <wilsonllucena@gmail.com>
2021-12-30 19:06:46 -0300

    refactor: configurações

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 export default {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+      '<rootDir>/src/**/*.ts',
+      '!<rootDir>/src/main/**'
+    ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
