c9adc130a5c199085fe6fff0fb1ad7d3edf17657
Wilson Lucena <wilsonllucena@gmail.com>
2022-01-05 21:05:35 -0300

    chore: configurações no mongo

--- src/utils/email-validator-adpater.ts
+++ src/utils/email-validator-adpater.ts
@@ -2,7 +2,6 @@ import { EmailValidator } from '../presentation/protocols/email-validator'
 import validator from 'validator'
 export class EmailValidatorAdapater implements EmailValidator {
   isValid (email: string): boolean {
-    console.log(email)
     return validator.isEmail(email)
   };
 }
